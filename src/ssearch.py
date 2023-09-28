2ed7ce04859a6f76d9447cc9f1b0e9a2df2c628e
sbarbett <shane@barbetta.me>
2023-09-28 17:33:16 -0400

    Adding functionality to handle token refresh

--- src/ssearch.py
+++ src/ssearch.py
@@ -7,30 +7,50 @@ import os
 from tqdm import tqdm
 
 BASE_URL = "https://api.ultradns.com"
-HEADERS = {"Content-Type": "application/json"}
-
 
 def get_primary_token(username=None, password=None, token=None):
     if token:
-        return token
+        print("Warning: A masquerade token cannot be refreshed upon expiry.")
+        return token, None
     auth_endpoint = f"{BASE_URL}/authorization/token"
     auth_data = {
         "grant_type": "password",
         "username": username,
         "password": password
     }
-    response = requests.post(auth_endpoint, data=auth_data)
+    response = requests.post(auth_endpoint, data=auth_data, headers={"Content-Type": "application/x-www-form-urlencoded"})
+    response.raise_for_status()
+    return response.json().get("accessToken"), response.json().get("refreshToken")
+
+def refresh_token(refresh_token):
+    auth_endpoint = f"{BASE_URL}/authorization/token"
+    auth_data = {
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token
+    }
+    response = requests.post(auth_endpoint, data=auth_data, headers={"Content-Type": "application/x-www-form-urlencoded"})
     response.raise_for_status()
     return response.json().get("accessToken")
 
-def get_subaccounts(token, offset=0):
+def make_request(method, url, token, refresh_token, **kwargs):
     headers = {"Authorization": f"Bearer {token}"}
+    response = requests.request(method, url, headers=headers, **kwargs)
+    if response.status_code == 403:
+        return response  # Return the response to handle the error at the caller level
+    if response.status_code == 401 and refresh_token:
+        new_token = refresh_token(refresh_token)
+        headers["Authorization"] = f"Bearer {new_token}"
+        response = requests.request(method, url, headers=headers, **kwargs)
+    response.raise_for_status()
+    return response
+
+def get_subaccounts(token, refresh_token, offset=0):
     subaccounts = []
     while True:
-        response = requests.get(f"{BASE_URL}/subaccounts?limit=1000&offset={offset}", headers=headers)
+        response = make_request("get", f"{BASE_URL}/subaccounts?limit=1000&offset={offset}", token, refresh_token)
         if response.status_code == 403 and 'do not have permissions' in response.text:
             print("Error: You do not have permissions to access sub-accounts. Ensure you're using a reseller account.")
-            exit(1)  # exit the script with a non-zero code indicating an error
+            exit(1)
         response.raise_for_status()
         data = response.json()
         subaccounts.extend(data.get("accounts", []))
@@ -49,7 +69,6 @@ def get_subaccount_token(account_name, primary_token):
         response.raise_for_status()
         return response.json()["accessToken"]
     except requests.HTTPError as e:
-        # Check if the error content mentions suspension
         if 'is suspended' in response.text:
             print(f"Skipping suspended sub-account: {account_name}")
             return None
@@ -74,7 +93,7 @@ def get_pools(zone_name, token, offset=0):
     pools = []
     while True:
         response = requests.get(f"{BASE_URL}/v2/zones/{urllib.parse.quote(zone_name)}/rrsets?q=kind:POOLS&limit=1000&offset={offset}", headers=headers)
-        if response.status_code == 404:  # No pool records found
+        if response.status_code == 404:
             break
         response.raise_for_status()
         data = response.json()
@@ -85,10 +104,10 @@ def get_pools(zone_name, token, offset=0):
     return pools
 
 def main(username=None, password=None, token=None, output_file=None, format="json"):
-    primary_token = get_primary_token(username, password, token)
-    subaccounts = get_subaccounts(primary_token)
-
+    primary_token, refresh_token_val = get_primary_token(username, password, token)
+    subaccounts = get_subaccounts(primary_token, refresh_token_val)
     data = [{"Sub Account Name": "", "Zone Name": "", "Pool Name": "", "Pool Type": ""}]
+    
     for account in tqdm(subaccounts, desc="Processing sub-accounts"):
         account_name = account.get("accountName")
         subaccount_token = get_subaccount_token(account_name, primary_token)
@@ -116,10 +135,10 @@ def main(username=None, password=None, token=None, output_file=None, format="jso
         if format == "json":
             print(json.dumps(data, indent=4))
         elif format == "csv":
-            csvwriter = csv.DictWriter(outfile, fieldnames=data[0].keys())
+            csvwriter = csv.DictWriter(sys.stdout, fieldnames=data[0].keys())
             csvwriter.writeheader()
             for row in data[1:]:
-                print(", ".join(row.values()))
+                csvwriter.writerow(row)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="UltraDNS Subaccounts Zones and Pools Finder")
@@ -135,8 +154,7 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
     
-    if args.token is None:
-        if args.username is None or args.password is None:
-            parser.error("When token is not provided, both username and password are required")
+    if args.token is None and (args.username is None or args.password is None):
+        parser.error("When token is not provided, both username and password are required")
     
     main(username=args.username, password=args.password, token=args.token, output_file=args.output_file, format=args.format)
